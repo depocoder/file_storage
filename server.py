5f5b68669996e11adfa3ef15f6a09bfff3f0ff65
Ivan <60302361+depocoder@users.noreply.github.com>
2021-10-20 21:08:40 +0300

    move html to file

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
+with open('templates/index.html') as file:
+    html = file.read()
+
 @app.get('/')
 def index_page():
-    return Response('Hello', media_type='text/html')
+    return Response(html, media_type='text/html')
