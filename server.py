685a23d4d4e3454e7f68c05b2e7ff2ce6e0569f8
depocoder <minecrafter0079@yandex.ru>
2021-10-27 22:46:52 +0300

    add cookies

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Form
+from typing import Optional
+
+from fastapi import FastAPI, Form, Cookie
 from fastapi.responses import Response
+
 
 app = FastAPI()
 
@@ -17,10 +20,22 @@ users = {
 }
 
 @app.get('/')
-def index_page():
+def index_page(username: Optional[str] = Cookie(default=None)):
     with open('templates/index.html') as file:
         html = file.read()
-    return Response(html, media_type='text/html')
+        response = Response(html, media_type='text/html')
+
+    if not username:
+        return response
+    user = users.get(username)
+    if user:
+        return Response(
+            f'You have alredy logged in your account. Your login {username}, your balance is {users[username]["balance"]}', media_type='text/html'
+            )
+    else:
+        response.delete_cookie('username')
+        return response
+
 
 @app.post('/login')
 def process_login_page(username : str = Form(...), password: str = Form(...)):
@@ -30,6 +45,8 @@ def process_login_page(username : str = Form(...), password: str = Form(...)):
             f'Incorrect password or username', media_type='text/html'
         )
     password = user.get('password')
-    return Response(
+    response = Response(
         f'your login {username}, your balance is {user["balance"]}', media_type='text/html'
         )
+    response.set_cookie(key='username', value=username)
+    return response
