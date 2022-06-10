562c589fe720b58b618a3745ef6cc277ba40ba74
depocoder <minecrafter0079@yandex.ru>
2022-06-10 21:38:04 +0300

    pep8 formatting

--- server.py
+++ server.py
@@ -11,13 +11,12 @@ from settings import SECRET_KEY, PASSWORD_SALT
 
 app = FastAPI()
 
-
 users = {
     'ivan@ya.ru': {
         'name': 'Ivan',
         'password': '1dcbc1c226e70ce7a71238787d7f70fdf906934dde11d9b662502a8bd8784926',
         'balance': 100_000
-    },    
+    },
     'pertr@ya.ru': {
         'name': 'pert',
         'password': '5dff562e6b11e6f23209a2d0054922074cae26e5a98ffef70c6baaf8892bea72',
@@ -45,9 +44,9 @@ def verify_password(username: str, password: str) -> bool:
 def hash_sign_cookie(cookie: str) -> str:
     """Возвращает подписанные данные"""
     return hmac.new(
-            SECRET_KEY.encode(), msg=cookie.encode(), digestmod=hashlib.sha256
-        ).hexdigest().upper()
-    
+        SECRET_KEY.encode(), msg=cookie.encode(), digestmod=hashlib.sha256
+    ).hexdigest().upper()
+
 
 @app.get('/')
 def index_page(username: Optional[str] = Cookie(default=None)):
@@ -65,28 +64,29 @@ def index_page(username: Optional[str] = Cookie(default=None)):
     user = users.get(valid_username)
     if user:
         return Response(
-            f'You have already logged in your account. Your login {valid_username}, your balance is {user["balance"]}', media_type='text/html'
-            )
+            f'You have already logged in your account. Your login {valid_username}, your balance is {user["balance"]}',
+            media_type='text/html'
+        )
     else:
         response.delete_cookie('username')
         return response
 
 
 @app.post('/login')
-def process_login_page(username : str = Form(...), password: str = Form(...)):
+def process_login_page(username: str = Form(...), password: str = Form(...)):
     if username not in users or not verify_password(username, password):
         return Response(
-        json.dumps({
-            "success": False,
-            "message": "Incorrect password or username",
-        }), media_type='application/json'
+            json.dumps({
+                "success": False,
+                "message": "Incorrect password or username",
+            }), media_type='application/json'
         )
     response = Response(
         json.dumps({
             "success": True,
             "message": "Login success",
         }), media_type='application/json'
-        )
+    )
     base64_username = base64.b64encode(username.encode()).decode()
     hashed_cookie = hash_sign_cookie(username)
     cookie_value = f"{base64_username}.{hashed_cookie}"
