86d3aa66565d061566fd9cc86eda722d558dbc50
depocoder <minecrafter0079@yandex.ru>
2022-06-15 17:31:30 +0300

    wemake-python-styleguide format

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
+"""Fast api main app."""
+import base64
+import hashlib
+import hmac
 import json
 from typing import Optional
-import hmac
-import hashlib
-import base64
 
-from fastapi import FastAPI, Form, Cookie
+from fastapi import Cookie, FastAPI, Form
 from fastapi.responses import Response
 
-from settings import SECRET_KEY, PASSWORD_SALT
+from settings import PASSWORD_SALT, SECRET_KEY
 
 app = FastAPI()
 
@@ -15,18 +16,16 @@ users = {
     'ivan@ya.ru': {
         'name': 'Ivan',
         'password': '1dcbc1c226e70ce7a71238787d7f70fdf906934dde11d9b662502a8bd8784926',
-        'balance': 100_000
     },
     'pertr@ya.ru': {
         'name': 'pert',
         'password': '5dff562e6b11e6f23209a2d0054922074cae26e5a98ffef70c6baaf8892bea72',
-        'balance': 0
-    }
+    },
 }
 
 
-def get_username_from_cookie(username_cookie: str) -> Optional[str]:
-    """Получение username'a из подписанной куки"""
+def get_username_from_cookie(username_cookie: str) -> str:
+    """Get username from signed cookie."""
     username, sign = username_cookie.split('.')
     username = base64.b64decode(username.encode()).decode()
     valid_sing = hash_sign_cookie(username)
@@ -36,22 +35,22 @@ def get_username_from_cookie(username_cookie: str) -> Optional[str]:
 
 def verify_password(username: str, password: str) -> bool:
     user = users.get(username)
-    password_hash = hashlib.sha256(f"{password} + {PASSWORD_SALT}".encode()).hexdigest().lower()
-    stored_password_hashed = user['password']
-    return password_hash == stored_password_hashed.lower()
+    hashed_password = hashlib.sha256(f'{password} + {PASSWORD_SALT}'.encode())
+    stored_password_hashed: str = user['password']
+    return hashed_password.hexdigest().lower() == stored_password_hashed.lower()
 
 
 def hash_sign_cookie(cookie: str) -> str:
-    """Возвращает подписанные данные"""
+    """Sign data."""
     return hmac.new(
-        SECRET_KEY.encode(), msg=cookie.encode(), digestmod=hashlib.sha256
+        SECRET_KEY.encode(), msg=cookie.encode(), digestmod=hashlib.sha256,
     ).hexdigest().upper()
 
 
 @app.get('/')
 def index_page(username: Optional[str] = Cookie(default=None)):
-    with open('templates/index.html') as file:
-        html = file.read()
+    with open('templates/index.html') as index_file:
+        html = index_file.read()
         response = Response(html, media_type='text/html')
     if not username:
         return response
@@ -64,31 +63,33 @@ def index_page(username: Optional[str] = Cookie(default=None)):
     user = users.get(valid_username)
     if user:
         return Response(
-            f'You have already logged in your account. Your login {valid_username}, your balance is {user["balance"]}',
-            media_type='text/html'
+            f'You have already logged in your account. Your login {valid_username}',
+            media_type='text/html',
         )
-    else:
-        response.delete_cookie('username')
-        return response
+    response.delete_cookie('username')
+    return response
 
 
 @app.post('/login')
 def process_login_page(username: str = Form(...), password: str = Form(...)):
+
     if username not in users or not verify_password(username, password):
+        incorrect_password_response = {
+            'success': False,
+            'message': 'Incorrect password or username',
+        }
         return Response(
-            json.dumps({
-                "success": False,
-                "message": "Incorrect password or username",
-            }), media_type='application/json'
+            json.dumps(incorrect_password_response), media_type='application/json',
         )
+    login_succes_response = {
+        'success': True,
+        'message': 'Login success',
+    }
     response = Response(
-        json.dumps({
-            "success": True,
-            "message": "Login success",
-        }), media_type='application/json'
+        json.dumps(login_succes_response), media_type='application/json',
     )
     base64_username = base64.b64encode(username.encode()).decode()
     hashed_cookie = hash_sign_cookie(username)
-    cookie_value = f"{base64_username}.{hashed_cookie}"
+    cookie_value = f'{base64_username}.{hashed_cookie}'
     response.set_cookie(key='username', value=cookie_value)
     return response
