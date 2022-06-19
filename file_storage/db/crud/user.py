8757e0e471ab7b7fab668aaea04f663bc05b7bd6
depocoder <minecrafter0079@yandex.ru>
2022-06-19 17:05:41 +0300

    feat(add new method): add new method to user crud - 'get_by_username'

--- file_storage/db/crud/user.py
+++ file_storage/db/crud/user.py
@@ -1,11 +1,13 @@
 """User CRUD."""
-import hashlib
-import hmac
+import logging
 import secrets
-from typing import Type, TypeVar
+from typing import Optional, Type, TypeVar
+
+from sqlalchemy import select
 
 from file_storage.db.crud.base import CRUDBase
 from file_storage.db.models.user import User
+from file_storage.web.api.cryptography import hash_password
 from file_storage.web.api.user.schema import (
     UserDeleteScheme,
     UserReadScheme,
@@ -14,6 +16,7 @@ from file_storage.web.api.user.schema import (
     UserWithPasswordScheme,
 )
 
+logger = logging.getLogger(__name__)
 ModelType = TypeVar("ModelType", bound=User)
 PASSWORD_SALT_LEN = 32
 
@@ -42,15 +45,7 @@ class UserCRUD(
         serialized_user = user_obj.dict()
         password = serialized_user.pop("password")
         passwod_salt = secrets.token_hex(PASSWORD_SALT_LEN)
-        hashed_password = (
-            hmac.new(
-                passwod_salt.encode(),
-                msg=password.encode(),
-                digestmod=hashlib.sha256,
-            )
-            .hexdigest()
-            .upper()
-        )
+        hashed_password = hash_password(password, passwod_salt)
         serialized_user.update(
             {"hashed_password": hashed_password, "password_salt": passwod_salt},
         )
@@ -59,3 +54,25 @@ class UserCRUD(
         self.session.add(db_user_obj)
         await self.session.flush()
         return user_obj
+
+    async def get_by_username(
+        self,
+        username: str,
+    ) -> Optional[ModelType]:
+        """Get user by username."""
+        query = select(self.model).where(self.model.username == username)
+        logger.info(
+            "A query has been sent to db to get an user",
+            extra={"username": username},
+        )
+        if (query_result := await self.session.execute(query)) is None:
+            logger.info(
+                "There is no user for the username",
+                extra={"username": username},
+            )
+            return None
+        logger.info(
+            "Succesfully got an user",
+            extra={"username": username},
+        )
+        return query_result.scalars().first()
